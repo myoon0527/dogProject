52387f79b41b78cfe9a7fc6a465981a9a3edf27e
yoon <myoon0527@naver.com>
2022-12-19 11:14:08 +0900

    yoon add commu

--- puppyHouse/src/main/java/com/cos/puppyHouse/Controller/CommuController.java
+++ puppyHouse/src/main/java/com/cos/puppyHouse/Controller/CommuController.java
@@ -1,15 +1,26 @@
 package com.cos.puppyHouse.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.cos.puppyHouse.Service.commuService;
+
 @Controller
 public class CommuController {
 	
-	@GetMapping("/commuBoard/commuMain")
-	public String commuMain()	{
+	@Autowired
+	private commuService commuService;
+	
+	@GetMapping({"/auth/commuBoard/commuMain"})
+	public String commuMain(Model model, @PageableDefault(size = 6, sort = "commuId", 
+	direction = Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("commu", commuService.list(pageable));
 		return "commuBoard/commuMain";
 	}
 	
@@ -18,11 +29,11 @@ public class CommuController {
 		return "commuBoard/commuSaveForm";
 	}
 	
-	/*
-	@GetMapping("/commuBoard/{id}")
+	
+	@GetMapping("/auth/commuBoard/{id}")
 	public String findById(@PathVariable int id, Model model) {
-		model.addAttribute("commuBoard", communityService.detail(id));
-		return "commuBoard/detail";
+		model.addAttribute("commuBoard", commuService.detail(id));
+		return "commuBoard/commuDetail";
 	}
-	*/
+	
 }
