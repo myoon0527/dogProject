4bd318741a90083e242199b931a85de8f20cce2b
yoon <myoon0527@naver.com>
2022-12-19 11:35:07 +0900

    sangha controller modify

--- puppyHouse/src/main/java/com/cos/puppyHouse/controller/CommuController.java
+++ puppyHouse/src/main/java/com/cos/puppyHouse/controller/CommuController.java
@@ -1,4 +1,4 @@
-package com.cos.puppyHouse.Controller;
+package com.cos.puppyHouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
